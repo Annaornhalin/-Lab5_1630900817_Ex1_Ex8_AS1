1d11387ca2979a6adcdaf894716ff5792e702756
Anna ornhalin <89762580+Annaornhalin@users.noreply.github.com>
2021-11-28 00:16:35 +0700

    Add files via upload

---
+++ ex2/js_ex2.js
@@ -0,0 +1,4 @@
+function myFunctionEx2_3() {
+    document.getElementById("ex2-3")
+    .innerHTML = "[External File] I'm great 😄.";
+  }
---
+++ ex4/js_ex4_2.js
@@ -0,0 +1,5 @@
+//JavaScript by Anna 
+//Ex4.2: JavaScript Numbers and Strings
+document.getElementById("ex4-2-1").innerHTML = 93.69; //Number
+document.getElementById("ex4-2-2").innerHTML = "Anna"; //String
+document.getElementById("ex4-2-3").innerHTML = "Anna score: " + 93.69; //Number + String
